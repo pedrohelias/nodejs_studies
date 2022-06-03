da187b12fc0eecdf65876198e977963082e35103
pedrohelias <pedrohelias95@hotmail.com>
2022-06-03 17:22:41 -0300

    fazendo uso do body-parser

---
+++ 9-banco_com_back_body/app.js
@@ -0,0 +1,56 @@
+//utilizaremos o body parser, para pegar dados do formulario
+//para instalar, é simples: npm install --save body-parser
+//handlebars x reactjs, pesquisar
+
+const bodyParser = require("body-parser")
+
+
+const express = require("express");
+const conection = express();
+
+//configurando body-parser
+conection.use(bodyParser.urlencoded({extend:false}))
+conection.use(bodyParser.json())
+
+//configurando sequelize
+const sequelize = require("sequelize");
+const db = new sequelize('database_app', 'root', 'pedro170995', {
+    host: 'localhost',
+    dialect: 'mysql'
+} );
+
+//configurando handlebars
+const handlebars = require("express-handlebars");
+const hbs = handlebars.create({defaultLayout:'main'});
+conection.engine('handlebars', hbs.engine);
+conection.set('view engine', 'handlebars')
+
+//criando rotas
+conection.get('/cad', function(req,res){
+    res.render('formulario')
+});
+
+conection.post('/add', function(req,res){ //post pois estamos utilizando o metodo post no formulario
+   
+    //paga pegar dados que estão vindo do formulario, basta escrever a estrutura a seguir:
+    let titulo = req.body.titulo; //o 'titulo' vem do atributo 'name' da tag do html, tanto em input quanto em textarea
+    let conteudo = req.body.conteudo; //o mesmo para ca.
+
+    res.send("Titulo: " + titulo + ", texto inserido: " + conteudo) //esse trecho vai inserir o texto que desejamos
+
+
+})
+
+
+
+//callback do banco de dados
+db.authenticate().then(function(){
+    console.log("banco conectado com sucesso!");
+}).catch(function(erro){
+    console.log("banco não conectado, devido ao erro: " + erro);
+});
+
+//callback do servidor
+conection.listen(8081,function(){
+    console.log("servidor subiu com sucesso!")
+});
