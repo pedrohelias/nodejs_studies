88e4aae7769895d57f7b9415a0377022e00b106b
pedrohelias <pedrohelias95@hotmail.com>
2022-06-07 18:07:35 -0300

    commit com mongodb

--- 10-revisao_ate_aqui/app.js
+++ 10-revisao_ate_aqui/app.js
@@ -16,53 +16,52 @@ const Post = require('./models/Post')
 
 //configurando handlebars
 const handlebars = require("express-handlebars");
-const { rsort } = require("semver");
-const hbs = handlebars.create({defaultLayout: 'main'}) //fazendo o handlebars saber qual o default dele
+const hbs = handlebars.create({ defaultLayout: 'main' }) //fazendo o handlebars saber qual o default dele
 Express.engine('handlebars', hbs.engine);
 Express.set('view engine', 'handlebars'); //configurando o handblars
 
 //definindo o body-parser para colher dados do front end - com express
-Express.use(bodyParser.urlencoded({extended:false}))
+Express.use(bodyParser.urlencoded({ extended: false }))
 Express.use(bodyParser.json());
 
 
 //criando as rotas e paginas, ja com funcções de coleta
 
-Express.get('/', function(req,res){
- //retorna tudo que há em Post
- Post.findAll().then(function(posts){ //findAll - encontre todos os posts
-    res.render('home', { //dentro dessas chaves conseguimos passsar qualquer valor para o handeblears
-        posts: posts
+Express.get('/', function (req, res) {
+    //retorna tudo que há em Post
+    Post.findAll().then(function (posts) { //findAll - encontre todos os posts
+        res.render('home', { //dentro dsas chaves conseguimos passsar qualquer valor para o handeblears
+            posts: posts
 
-    })    
- }).catch(); //vai retornar todos os bancos que existem na tabela post. 
- //temos que passar os posts para dentro da view. E para isso usamos o then. Vai realiazr o efeito sempre que função for positiva. Nesse caso ele vai receber como parametro todos os posts
+        })
+    }).catch(); //vai retornar todos os bancos que existem na tabela post. 
+    //temos que passar os posts para dentro da view. E para isso usamos o then. Vai realiazr o efeito sempre que função for positiva. Nesse caso ele vai receber como parametro todos os posts
 
-//É possível definir a forma como é mostrado o dado, se é crescente ou decrescente. Basta fazer Post.all{{order:[['id','DESC']]}} para decrescente... ou vice versa
-    
+    //É possível definir a forma como é mostrado o dado, se é crescente ou decrescente. Basta fazer Post.all{{order:[['id','DESC']]}} para decrescente... ou vice versa
+
 })
 
 
-Express.get('/cad', function(req,res){
+Express.post('/cad', function (req, res) {
     res.render('formulario') //vai renderizar 'formulario' em /cad
 })
 
-Express.post('/add', function(req,res){ //atraves dessa funcao vamos pegar o que foi colocado no formulario e adicionar no banco
-   Post.create({ //aqui vamos pegar a variaveis Post e cria conteudo a partir do que foi estabelecido nela.
-       titulo: req.body.titulo,
-       conteudo: req.body.conteudo
-       //queremos saber se houve sucesso ou naõ na criação do post, então utilizando .then e .catch
-   }).then(function(){
-       
-       res.redirect('/') //caso tenha sucesso vai ser redicionado para uma pagina com todos os posts
-   }).catch(function(erro){
-       res.send('erro na criação do post: ' + erro)
-   })
+Express.post('/add', function (req, res) { //atraves dessa funcao vamos pegar o que foi colocado no formulario e adicionar no banco
+    Post.create({ //aqui vamos pegar a variaveis Post e cria conteudo a partir do que foi estabelecido nela.
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo
+        //queremos saber se houve sucesso ou naõ na criação do post, então utilizando .then e .catch
+    }).then(function () {
+
+        res.redirect('/') //caso tenha sucesso vai ser redicionado para uma pagina com todos os posts
+    }).catch(function (erro) {
+        res.send('erro na criação do post: ' + erro)
+    })
 })
 
 
 
 
-Express.listen(8081, function(){
+Express.listen(8081, function () {
     console.log("servidor subiu com sucesso em: https://localhost:8081/")
 })
---
+++ 11-mongoDB/mongo.md
@@ -0,0 +1,12 @@
+- O mongoDB é um banco de dados em código aberto, gratuito e de alta performance, sem esquemcas e orientado a documentos
+- O mysql é estruturado via tabela. Dentro do MongoDB não existem tabelas. Cada dado é salvo em um documento diferente, porexemplo:
+
+  {
+  name: 'al',
+  age: 18,
+  status: 'D',
+  groups: ['politics', 'news']
+
+  }
+
+É simplesmente a esturtura de um json, a estrutura de objetos do java script. É possível salvar assim. Ele vai salvar tudo numa collection.
